170f71a0ea556b3855689ad24222253217650712
Edinaldo Silva <84786877+onworkconsultoria@users.noreply.github.com>
2024-04-10 14:43:47 -0300

    Add files via upload

---
+++ tictactoe-android/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tictactoe_android
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void single_player_clicked(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new single_player();
+            Console.WriteLine("1 jogador Clicado");
+        }
+
+        public void multi_player_clicked(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new Multi_Player();
+            Console.WriteLine("2 Jogadores Clicados");
+        }
+
+        public void Multi_Each_Btn_Clicked(object sender, EventArgs args)
+        {
+
+        }
+    }
+}
